cb5956808aa9007ccb6752aa52f56983f9378a08
webblurt <webblurt@gmail.com>
2026-03-09 15:37:48 +0200

    fix

--- apps/backend/internal/wardrobe/domain/replicate.go
+++ apps/backend/internal/wardrobe/domain/replicate.go
@@ -5,7 +5,7 @@ type ReplicateUploadResp struct {
 }
 
 type ReplicatePredictionResp struct {
-	ID     string   `json:"id"`
-	Status string   `json:"status"`
-	Output []string `json:"output"`
+	ID     string `json:"id"`
+	Status any    `json:"status"`
+	Output any    `json:"output"`
 }
